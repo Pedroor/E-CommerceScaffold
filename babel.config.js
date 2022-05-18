5b0196d6fce023c754e82315313337713110ac47
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-05-18 14:38:07 -0300

    feat: create splash ios and android

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        alias: {
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@constants": "./src/constants",
+          "@configs": "./src/configs",
+          "@helpers": "./src/helpers",
+          "@screens": "./src/screens",
+          "@services": "./src/services",
+          "@store": "./src/store",
+        },
+      },
+    ],
+  ],
 };
