1ece4a98f71102e8cd92e44ab4a17f54eb1001ee
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-05-18 13:08:24 -0300

    config: create RN project

---
+++ ios/Pods/Target Support Files/Pods-EcommerceScaffold-EcommerceScaffoldTests/Pods-EcommerceScaffold-EcommerceScaffoldTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_EcommerceScaffold_EcommerceScaffoldTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_EcommerceScaffold_EcommerceScaffoldTestsVersionString[];
+
---
+++ ios/Pods/Target Support Files/Pods-EcommerceScaffold/Pods-EcommerceScaffold-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EcommerceScaffold : NSObject
+@end
+@implementation PodsDummy_Pods_EcommerceScaffold
+@end
