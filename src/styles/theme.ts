8986edcaf701f2c20e8d64dd928ac065556b8ae3
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-05-18 20:09:34 -0300

    styles: config theme and fonts

---
+++ src/styles/theme.ts
@@ -0,0 +1,48 @@
+export default {
+  colors: {
+    black: "#000000",
+    white: "#f0f2f5",
+    grey: "##B4B4B6",
+    primary: "#8775FE",
+    secondary: "##ECE9FF",
+    subtitle: "#85868A",
+    background: "#E5E5E5",
+    semiGrey: "#F4F5F4",
+    red: "#FA6555",
+    green: "#41D168",
+    title: "#f2231f",
+  },
+
+  textVariants: {
+    section: {
+      fontSize: "24px",
+      fontFamily: "WorkSans-SemiBold",
+      lineHeight: "28px",
+    },
+    header: {
+      fontSize: "18px",
+      fontFamily: "WorkSans-Bold",
+      lineHeight: "22px",
+    },
+    label: {
+      fontSize: "8px",
+      fontFamily: "WorkSans-Bold",
+      lineHeight: "9px",
+    },
+    title: {
+      fontSize: "14px",
+      fontFamily: "WorkSans-Bold",
+      lineHeight: "16px",
+    },
+    subtitle: {
+      fontSize: "10px",
+      fontFamily: "WorkSans-Medium",
+      lineHeight: "12px",
+    },
+    price: {
+      fontSize: "20px",
+      fontFamily: "WorkSans-Bold",
+      lineHeight: "28px",
+    },
+  },
+} as const;
