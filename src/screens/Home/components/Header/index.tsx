7a875144bae6943633927c6651f30ee7cec99831
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-05-22 12:05:58 -0300

    feat: cart bag amount

--- src/screens/Home/components/Header/index.tsx
+++ src/screens/Home/components/Header/index.tsx
@@ -1,11 +1,13 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import CartBag from "../../../../assets//BAG.png";
+import useCart from "../../../../store/useCart";
 
 import * as S from "./styles";
 
 const Header: React.FC = () => {
   const navigation = useNavigation();
+  const { cartProducts } = useCart();
 
   const handleNavigateToCart = () => {
     console.log("navigate");
@@ -15,6 +17,9 @@ const Header: React.FC = () => {
     <S.RowContainer>
       <S.PageTitle>Produtos</S.PageTitle>
       <S.TouchableBox onPress={() => handleNavigateToCart()}>
+        <S.IndicationCartAmount>
+          <S.TextRed>{cartProducts.length}</S.TextRed>
+        </S.IndicationCartAmount>
         <S.Bag source={CartBag} />
       </S.TouchableBox>
     </S.RowContainer>
--- src/screens/Home/components/Header/styles.ts
+++ src/screens/Home/components/Header/styles.ts
@@ -18,3 +18,23 @@ export const Bag = styled.Image`
 `;
 
 export const TouchableBox = styled.TouchableOpacity``;
+
+export const IndicationCartAmount = styled.View`
+  position: absolute;
+  top: -5px;
+  right: -10px;
+  width: 20px;
+  height: 20px;
+  z-index: 2;
+  border-radius: 60px;
+  background-color: ${(props) => props.theme.colors.white};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextRed = styled.Text`
+  color: ${(props) => props.theme.colors.primary};
+  text-align: center;
+  align-self: center;
+  ${(props) => props.theme.textVariants.titleHeader}
+`;
