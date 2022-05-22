ed95ef57820e2bb3a140c1fad92c3f5d886becb5
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-05-22 12:14:57 -0300

    feat: button of footer of list

--- src/screens/Home/components/ProductList/index.tsx
+++ src/screens/Home/components/ProductList/index.tsx
@@ -1,13 +1,18 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { ListRenderItem } from "react-native";
 import { ProductProps } from "../../../../@types/interfaces";
+import Button from "../../../../components/Button";
+import useCart from "../../../../store/useCart";
 import { useProducts } from "../../../../store/useProducts";
 import CardMedium from "../CardMedium";
 
 import * as S from "./styles";
 
 const ProductList: React.FC = () => {
+  const { navigate } = useNavigation();
   const { productsList } = useProducts();
+  const { cartProducts } = useCart();
   const renderItem: ListRenderItem<ProductProps> = ({ item }) => {
     return <CardMedium product={item} />;
   };
@@ -19,6 +24,18 @@ const ProductList: React.FC = () => {
         renderItem={renderItem}
         keyExtractor={(item: ProductProps) => item.id}
         showsVerticalScrollIndicator={false}
+        ListFooterComponent={() => {
+          if (cartProducts.length !== 0) {
+            return (
+              <Button
+                title="IR PARA O CARRINHO"
+                onPress={() => navigate("Cart")}
+              />
+            );
+          } else {
+            return <></>;
+          }
+        }}
       />
     </S.Container>
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,54 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CategoryList from "./components/CategoryList";
 
 import * as S from "./styles";
 import NewsSection from "./components/NewsSection";
 import ProductSection from "./components/ProductSection";
 import Header from "./components/Header";
-import ModalizeButton from "./components/ModalizeButton";
-import { ProductProps } from "../../@types/interfaces";
-import { getAllProducts } from "../../services/products";
-import { getAllCategories } from "../../services/categories";
 
 const Home: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [newsProducts, setNewsProducts] = useState<ProductProps[]>();
-  const [products, setProducts] = useState<ProductProps[]>();
-  const [categories, setCategories] = useState<string[]>();
-
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const productsResponse = await getAllProducts();
-        setProducts(productsResponse);
-        setNewsProducts(productsResponse.slice(0, 5));
-      } catch (error) {
-        throw error;
-      }
-    }
-    fetchProducts();
-  }, []);
-
-  useEffect(() => {
-    async function fetchCategories() {
-      try {
-        const categoriesResponse = await getAllCategories();
-        setCategories(categoriesResponse);
-      } catch (error) {
-        throw error;
-      }
-    }
-    fetchCategories();
-  }, []);
-
   return (
     <S.SafeContainer>
       <S.Container>
         <Header />
         <CategoryList />
         <NewsSection />
-        <ProductSection products={products} />
-        <ModalizeButton />
+        <ProductSection />
       </S.Container>
     </S.SafeContainer>
   );
