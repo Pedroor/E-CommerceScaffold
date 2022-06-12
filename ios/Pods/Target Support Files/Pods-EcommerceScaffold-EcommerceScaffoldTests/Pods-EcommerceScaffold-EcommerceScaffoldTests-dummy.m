af44ca703cf0a4e1142517c60871c875737d4b90
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2022-06-12 10:38:14 -0300

    fix: remove pods of git and adjust .gitignore

--- ios/Pods/Target Support Files/Pods-EcommerceScaffold-EcommerceScaffoldTests/Pods-EcommerceScaffold-EcommerceScaffoldTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EcommerceScaffold_EcommerceScaffoldTests : NSObject
-@end
-@implementation PodsDummy_Pods_EcommerceScaffold_EcommerceScaffoldTests
-@end
--- ios/Pods/Target Support Files/Pods-EcommerceScaffold/Pods-EcommerceScaffold-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EcommerceScaffold : NSObject
-@end
-@implementation PodsDummy_Pods_EcommerceScaffold
-@end
